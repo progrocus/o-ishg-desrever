519379de3a07c5b44570fe5d2fc0c4d34a04b289
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2015-09-11 16:54:47 -0400

    Added Goal tile and Switch

--- js/script.js
+++ js/script.js
@@ -8,32 +8,39 @@ window.onload = function(){
 			ch = 400,
 			lastKey,
 			currentLevel = 0,
-			flipped = false;
+			flipped = false,
+			switched = 1,
+			goal,
+			start;
 	
 	function doKeyDown(e){
 		if(e.keyCode == 37){//left
-			player.update("x", -50);
+			player.update("x", -50 * switched);
 			lastKey = "left";
 		}else if(e.keyCode == 38){//up
-			player.update("y", -50);
+			player.update("y", -50 * switched);
 			lastKey = "up";
 		}else if(e.keyCode == 39){//right
-			player.update("x", 50);
+			player.update("x", 50 * switched);
 			lastKey = "right";
 		}else if (e.keyCode == 40){//down
-			player.update("y", 50);
+			player.update("y", 50 * switched);
 			lastKey = "down";
 		}else{
 			//do nothing
 		}
 	}
 	
+	/* LEVELS DICTIONARY */
 	var levels = [
 	  {
 	    name: "Level 1",
 	    walls: [2,3,4],
 	    triggers: [5],
-	    floors: [10,11,12]
+	    floors: [10,11,12],
+	    switches: [19],
+	    start: 0,
+	    goal: 63
 	  },
 	  {
 	    name: "Level 2",
@@ -47,11 +54,18 @@ window.onload = function(){
 	var walls = [];
 	var triggers = [];
 	var floors = [];
-	
+	var switches = [];
+
 	function Tile(I){
 	  I.w = 50;
 	  I.h = 50;
-	  I.colors = {"wall":"848887", "floor": "white", "trigger": "#F7EF99"};
+	  I.colors = {
+	  	"wall":"#848887", 
+	  	"floor": "white", 
+	  	"trigger": "#F7EF99", 
+	  	"switch": "#43ADF5",
+	  	"goal": "#23CE6B"
+	  };
 	  I.draw = function(){
 	    c.fillStyle = this.colors[this.type];
 	    roundedRect(c, this.x + 2, this.y + 2, this.w - 4, this.h - 4, 9);
@@ -64,13 +78,10 @@ window.onload = function(){
       c.moveTo(x, 0);
       c.lineTo(x, ch);
 		}
-
-
     for (var x = 0; x <= ch; x += 50) {
       c.moveTo(0, x);
       c.lineTo(cw, x);
 		}
-
 		c.strokeStyle = "black";
 		c.stroke();
 	}
@@ -104,6 +115,7 @@ window.onload = function(){
 	  walls.length = 0;
 	  floors.length = 0;
 	  triggers.length = 0;
+	  switches.length = 0;
 	  levels[currentLevel].walls.forEach(function(i){
 	    walls.push(Tile({
 	      x: (i%8 * 50),
@@ -111,20 +123,27 @@ window.onload = function(){
 	      type: "wall"
 	    }));
 	  });
-	  levels[0].triggers.forEach(function(i){
+	  levels[currentLevel].triggers.forEach(function(i){
 	    triggers.push(Tile({
 	      x: (i%8 * 50),
 	      y: (Math.floor(i/8) * 50),
 	      type: "trigger"
 	    }))
 	  });
-	  levels[0].floors.forEach(function(i){
+	  levels[currentLevel].floors.forEach(function(i){
 	    floors.push(Tile({
 	      x: (i%8 * 50),
 	      y: (Math.floor(i/8) * 50),
 	      type: "floor"
 	    }))
 	  });
+	  levels[currentLevel].switches.forEach(function(i){
+	  	switches.push(Tile({
+	  		x: (i%8 * 50),
+	      y: (Math.floor(i/8) * 50),
+	      type: "switch"
+	  	}))
+	  })
 	}
 	
 	function flipMap(){
@@ -132,6 +151,7 @@ window.onload = function(){
 	  walls.length = 0;
 	  floors.length = 0;
 	  triggers.length = 0;
+	  switches.length = 0;
 	  levels[currentLevel].walls.forEach(function(i){
 	    floors.push(Tile({
 	      x: (i%8 * 50),
@@ -146,13 +166,24 @@ window.onload = function(){
 	      type: "wall" 
 	    }))
 	  });
-	  levels[0].triggers.forEach(function(i){
+	  levels[currentLevel].triggers.forEach(function(i){
 	    triggers.push(Tile({
 	      x: (i%8 * 50),
 	      y: (Math.floor(i/8) * 50),
 	      type: "trigger"
 	    }))
 	  });
+	  levels[currentLevel].switches.forEach(function(i){
+	  	switches.push(Tile({
+	  		x: (i%8 * 50),
+	      y: (Math.floor(i/8) * 50),
+	      type: "switch"
+	  	}))
+	  });
+	}
+
+	function switchKeys(){
+
 	}
 	
 	/* UTILITY FUNCTIONS */
@@ -184,13 +215,13 @@ window.onload = function(){
 			if (collides(wall, player)) {
 			  console.log("collision");
 			  if(lastKey == "left"){
-			    player.update("x", 50);
+			    player.update("x", 50 * switched);
 			  }else if(lastKey == "up"){
-			    player.update("y", 50);
+			    player.update("y", 50 * switched);
 			  }else if(lastKey == "right"){
-			    player.update("x", -50);
+			    player.update("x", -50 * switched);
 			  }else if(lastKey == "down"){
-			    player.update("y", -50);
+			    player.update("y", -50 * switched);
 			  }else{
 			    //Do nothing
 			  }	
@@ -198,11 +229,19 @@ window.onload = function(){
 		});
 		triggers.forEach(function(trigger){
 		  if(collides(trigger, player) && !flipped){
-		    //Switch the levels
+		    console.log("triggered");
 		    flipMap()
 		    //Take care of infinite loop
 		  }
+		});
+		switches.forEach(function(s){
+			if(collides(s, player) && switched == 1){
+				switched = -1;
+			}
 		})
+		if(collides(goal,player)){
+			console.log("Level Complete");
+		}
 		
 	}
 
@@ -218,19 +257,32 @@ window.onload = function(){
 		canvas.width = canvas.width;
 
 		drawBoard();
-		walls.forEach(function(wall){
-		  wall.draw();
+		walls.forEach(function(w){
+		  w.draw();
 		});
-		triggers.forEach(function(trigger){
-		  trigger.draw();
+		triggers.forEach(function(t){
+		  t.draw();
 		});
+		switches.forEach(function(s){
+			s.draw();
+		})
+		goal.draw();
 		player.draw();
 	}
 
 	var FPS = 30;
 
-	function start_game(){
+	function start_level(){
 		drawMap();
+		goal = Tile({
+			x: (levels[currentLevel].goal % 8 * 50),
+			y: (Math.floor(levels[currentLevel].goal /8) * 50),
+			type: "goal"
+		})
+	}
+
+	function start_game(){
+		start_level();
 		refreshIntervalId = setInterval(function(){
 			update();
 			draw();
