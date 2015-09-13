3e1fcc3f7193a50b7bf7cfbde9f4871611bc948b
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2015-09-12 22:03:39 -0400

    Levels implemented

--- js/script.js
+++ js/script.js
@@ -35,18 +35,48 @@ window.onload = function(){
 	var levels = [
 	  {
 	    name: "Level 1",
-	    walls: [2,3,4],
-	    triggers: [5],
-	    floors: [10,11,12],
-	    switches: [19],
+	    walls: [],
+	    triggers: [],
+	    floors: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62],
+	    switches: [],
 	    start: 0,
 	    goal: 63
 	  },
 	  {
 	    name: "Level 2",
-	    walls: [19,20,21],
-	    triggers: [6, 34],
-	    floors: []
+	    walls: [1,2,3,4,5,6,7,16,19,20,21,22,30,38,46,62,24,32,40,48,56,57,58,59,60,61,49,50,51,52,41,42,43,33,34,25,37,29,28,47,39,31,15,14,13,12,11,10,23],
+	    triggers: [],
+	    floors: [0,8,9,17,18,26,27,35,36,44,45,53,54,55],
+	    switches: [],
+	    start: 0,
+	    goal: 63
+	  },
+	  {
+	    name: "Level 3",
+	    walls: [8,9,10,11,12,13,14,15,23,31,39,47,55],
+	    triggers: [7],
+	    floors: [0,1,2,3,4,5,6,14,22,30,38,46,54,62],
+	    switches: [],
+	    start: 0,
+	    goal: 63
+	  },
+	  {
+	    name: "Level 4",
+	    walls: [1,9,17,25,26,27,28,29,30,3140,41,42,43,44,45,46,54,62,40,31],
+	    triggers: [],
+	    floors: [0,8,16,24,32,33,34,35,36,37,38,39,47,55],
+	    switches: [35],
+	    start: 0,
+	    goal: 63
+	  },
+	  {
+	    name: "Level 4",
+	    walls: [8,9,10,11,13,14,15,28,36,44,52,51,50,49],
+	    triggers: [12],
+	    floors: [0,1,2,3,4,5,6,7,16,17,18,19,21,22,23,27,35,43,42,41,40,56,57,58,59,60,61,53,45,37,29],
+	    switches: [20],
+	    start: 0,
+	    goal: 48
 	  }
 	];
 	
@@ -61,7 +91,7 @@ window.onload = function(){
 	  I.h = 50;
 	  I.colors = {
 	  	"wall":"#848887", 
-	  	"floor": "white", 
+	  	"floor": "#BFEFFF", 
 	  	"trigger": "#F7EF99", 
 	  	"switch": "#43ADF5",
 	  	"goal": "#23CE6B"
@@ -88,7 +118,6 @@ window.onload = function(){
 
 	/* PLAYER  */
 	var player = {
-		bcolor: "white",
 		color: "#A846A0",
 		width: 50,
 		height: 50,
@@ -97,7 +126,7 @@ window.onload = function(){
 
 		draw: function(){
       c.fillStyle = this.color;
-			roundedRect(c, this.x+5, this.y+5, this.width -10, this.height -10, 10);
+			roundedRect(c, this.x+7, this.y+7, this.width -14, this.height -14, 10);
 		},
 
 		update: function(direction, value){
@@ -182,10 +211,6 @@ window.onload = function(){
 	  });
 	}
 
-	function switchKeys(){
-
-	}
-	
 	/* UTILITY FUNCTIONS */
 	
 	// Draw a rounded rectangle utility function
@@ -240,7 +265,12 @@ window.onload = function(){
 			}
 		})
 		if(collides(goal,player)){
+			currentLevel+=1;
+			player.x = (levels[currentLevel].start % 8) * 50;
+			player.y = Math.floor(levels[currentLevel].start / 8) * 50;
+			console.log(currentLevel);
 			console.log("Level Complete");
+			start_level();
 		}
 		
 	}
@@ -256,16 +286,20 @@ window.onload = function(){
 	function draw(){
 		canvas.width = canvas.width;
 
-		drawBoard();
-		walls.forEach(function(w){
-		  w.draw();
-		});
+		//drawBoard();
+		
 		triggers.forEach(function(t){
 		  t.draw();
 		});
+		floors.forEach(function(f){
+			f.draw();
+		})
 		switches.forEach(function(s){
 			s.draw();
-		})
+		});
+		walls.forEach(function(w){
+		  w.draw();
+		});
 		goal.draw();
 		player.draw();
 	}
@@ -273,6 +307,7 @@ window.onload = function(){
 	var FPS = 30;
 
 	function start_level(){
+		switched = 1;
 		drawMap();
 		goal = Tile({
 			x: (levels[currentLevel].goal % 8 * 50),
@@ -287,6 +322,10 @@ window.onload = function(){
 			update();
 			draw();
 		}, 1000 / FPS);
+	}
+
+	function end_game(){
+
 	}
 
 	start_game();
